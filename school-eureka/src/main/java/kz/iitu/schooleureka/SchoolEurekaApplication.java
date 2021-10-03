1c267512eca28bf4c18487aa83667e10b3673f15
turgan6ek <turgan6ek@gmail.com>
2021-10-03 17:52:30 +0600

    eureka

---
+++ school-eureka/src/main/java/kz/iitu/schooleureka/SchoolEurekaApplication.java
@@ -0,0 +1,15 @@
+package kz.iitu.schooleureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SchoolEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolEurekaApplication.class, args);
+    }
+
+}
---
+++ school-eureka/src/test/java/kz/iitu/schooleureka/SchoolEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.schooleureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

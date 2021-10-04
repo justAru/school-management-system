1234245d470d6b9ecaf05a671b09ad0961b720e1
turgan6ek <turgan6ek@gmail.com>
2021-10-04 15:08:42 +0600

    admin-eureka

--- school-administration-service/src/main/java/iitu/kz/schooladministrationservice/SchoolAdministrationServiceApplication.java
+++ school-administration-service/src/main/java/iitu/kz/schooladministrationservice/SchoolAdministrationServiceApplication.java
@@ -2,8 +2,10 @@ package iitu.kz.schooladministrationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SchoolAdministrationServiceApplication {
 
 	public static void main(String[] args) {

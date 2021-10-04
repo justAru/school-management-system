dc6368c5297ef0538fd72df7120afe00f2ab46a9
progaza <fakhrudinov.azamat@gmail.com>
2021-10-04 16:05:17 +0600

    configure eureka

--- school-schedule-service/src/main/java/iitu/kz/schoolscheduleservice/SchoolScheduleServiceApplication.java
+++ school-schedule-service/src/main/java/iitu/kz/schoolscheduleservice/SchoolScheduleServiceApplication.java
@@ -2,8 +2,10 @@ package iitu.kz.schoolscheduleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SchoolScheduleServiceApplication {
 
 	public static void main(String[] args) {
